1fd2bc81e990b176808cdf327bc5ab04e7b1f803
Sam Johnson <sam@durosoft.com>
2020-03-12 02:57:21 -0400

    WIP

--- src/video-manager.cr
+++ src/video-manager.cr
@@ -1,6 +1,47 @@
-# TODO: Write documentation for `Video::Manager`
-module Video::Manager
-  VERSION = "0.1.0"
+require "json"
 
-  # TODO: Put your code here
+VALID_VIDEO_EXTENSIONS = Set.new(["mkv", "mp4", "avi", "wmv", "mov", "mpg", "mpeg", "flv", "swf"])
+
+abstract class VideoFile
+  property path : String
+  property encoding_started_at : Time | Nil
+  property encoding_finished_at : Time | Nil
+  property encoding_options : String
+  property sha256 : String
+
+  abstract def refresh
+  abstract def exists? : Bool
+  abstract def name : String
+  abstract def size : UInt64
+  abstract def created_at : Time
+  abstract def modified_at : Time
+  abstract def copy_to_local(local_path : String)
+  abstract def replace_with_local(src_path : String)
+end
+
+class LocalVideoFile < VideoFile
+
+end
+
+abstract class VideoDirectory
+  property path : String
+
+  abstract def files : Array(VideoFile)
+end
+
+class LocalVideoDirectory < VideoDirectory
+  def initialize(@path)
+    @files = Array(LocalVideoFile).new
+    Dir.children(@path).each do |filename|
+      next unless 
+    end
+  end
+
+  def files
+
+  end
+end
+
+class SambaVideoDirectory < VideoDirectory
+
 end
